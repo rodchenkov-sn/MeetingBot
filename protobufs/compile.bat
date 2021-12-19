94d94920bc58267f429cd45aa0ab23da9839ade2
Arthur Mkrtchyan <almond.pitch@mail.ru>
2021-12-19 16:15:59 +0300

    Remove: unnecessary protos

--- protobufs/compile.bat
+++ protobufs/compile.bat
@@ -1,5 +1,4 @@
 @echo off
 python -m grpc_tools.protoc -I%~dp0src --python_out=%~dp0../tg_api_adapter/src --grpc_python_out=%~dp0../tg_api_adapter/src %~dp0src\user_message.proto
 python -m grpc_tools.protoc -I%~dp0src --python_out=%~dp0../frontend_server/src --grpc_python_out=%~dp0../frontend_server/src  %~dp0src/user_message.proto
-python -m grpc_tools.protoc -I%~dp0src --python_out=%~dp0../frontend_server/src --grpc_python_out=%~dp0../frontend_server/src  %~dp0src/localized_string.proto
 echo Done.
