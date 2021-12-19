4367cf00ef5211def830972aa9541111a947e35c
Arthur Mkrtchyan <almond.pitch@mail.ru>
2021-12-19 19:21:13 +0300

    Add: team adding logics

--- frontend_server/src/frontend_server.py
+++ frontend_server/src/frontend_server.py
@@ -1,33 +1,77 @@
 from concurrent import futures
+from dataclasses import dataclass
 
 import grpc
+import yaml
 
 import user_message_pb2 as um
 import user_message_pb2_grpc as ums
 
-import localized_string_pb2 as ls
-import localized_string_pb2_grpc as lss
+with open('config.yml', 'r') as config_file:
+    config = yaml.safe_load(config_file)
+
+states = dict()
+
+
+@dataclass
+class State:
+    command: str
+    action: str
+    value: int = None
 
 
 class UserMessageHandler(ums.UserMessageHandlerServicer):
     def HandleMessage(self, request, context):
-        print(get_localized_string(1))
-        if request.text != "":
-            yield um.ServerResponse(user_id=request.user_id, text=request.text)
-        elif request.document.file_name != "":
-            yield um.ServerResponse(user_id=request.user_id, text=request.document.file_name)
+        user_id = request.user_id
+        text = request.text
+        print(f'\nuser_id: {user_id}')
+        print(f'text: {text}')
+
+        if text == '/create_team':
+            set_state(user_id, 'create_team:enter_name')
+            yield um.ServerResponse(
+                user_id=user_id,
+                text='Enter name'
+            )
+        elif user_id in states and states[user_id] == 'create_team:enter_name':
+            # todo Backend.CreateTeam
+            remove_state(user_id)
+            yield um.ServerResponse(
+                user_id=user_id,
+                text=f'Team {text} created'
+            )
+        else:
+            yield um.ServerResponse(
+                user_id=user_id,
+                text='Sorry, bot does not understand you'
+            )
 
 
-def get_localized_string(string_id):
-    channel = grpc.insecure_channel('localhost:8000')
-    stub = lss.LocalizationHandlerStub(channel)
-    response = stub.getString(ls.StringRequest(string_id=string_id))
-    return response.value
+def set_state(user_id, state):
+    states[user_id] = state
+    print(f'Set state (user_id: {user_id}, state: {states[user_id]})')
+
+
+def remove_state(user_id):
+    print(f'Remove state (user_id: {user_id}, state: {states[user_id]})')
+    del states[user_id]
+
+
+def state_to_str(state: State):
+    value = ''
+    if state.value is not None:
+        value = f':{state.value}'
+    return f'{state.command}:{state.action}{value}'
+
+
+def str_to_state(string: str):
+    # todo
+    return State('', '')
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     ums.add_UserMessageHandlerServicer_to_server(UserMessageHandler(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port(config['frontend_server_url'])
     server.start()
     server.wait_for_termination()
--- protobufs/compile.bat
+++ protobufs/compile.bat
@@ -1,4 +1,5 @@
 @echo off
 python -m grpc_tools.protoc -I%~dp0src --python_out=%~dp0../tg_api_adapter/src --grpc_python_out=%~dp0../tg_api_adapter/src %~dp0src\user_message.proto
 python -m grpc_tools.protoc -I%~dp0src --python_out=%~dp0../frontend_server/src --grpc_python_out=%~dp0../frontend_server/src  %~dp0src/user_message.proto
+python -m grpc_tools.protoc -I%~dp0src --python_out=%~dp0../frontend_server/src --grpc_python_out=%~dp0../frontend_server/src  %~dp0src/frontend_message.proto
 echo Done.
