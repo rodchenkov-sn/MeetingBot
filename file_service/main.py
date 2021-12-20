a3abc455300d3d3e55b71a74c2b4c2f260b44df1
Alexander Kraynov <kraynovsasha@gmail.com>
2021-12-21 00:47:59 +0300

    Add file service

---
+++ file_service/main.py
@@ -0,0 +1,27 @@
+import asyncio
+import logging
+from concurrent import futures
+
+import grpc
+
+import bd
+import file_repo_service_pb2_grpc
+import filehost
+import file_repo_servicer
+
+
+async def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    file_repo_service_pb2_grpc.add_FileRepoServiceServicer_to_server(
+        file_repo_servicer.FileRepoServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    filehost.api.run(debug=True, port=10800)
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    asyncio.get_event_loop().run_until_complete(serve())
+
+
