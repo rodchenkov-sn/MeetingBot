a1970d6b836b14914157868927ff168a65667c2e
rodchenkov-sn <serod2015@hotmail.com>
2021-12-20 14:21:53 +0300

    team policy backend support

--- backend_server/src/team_policy.py
+++ backend_server/src/team_policy.py
@@ -5,10 +5,16 @@ class TeamPolicy:
     def __init__(self):
         self.allow_users_to_create_meetings = True
         self.need_approve_for_meeting_creation = False
+        self.propagate_policy = False
+        self.parent_visible = True
+        self.propagate_admin = True
 
 
 def policy_from_msg(msg: bs.TeamPolicy) -> TeamPolicy:
     policy = TeamPolicy()
     policy.need_approve_for_meeting_creation = msg.needApproveForMeetingCreation
     policy.allow_users_to_create_meetings = msg.allowUsersToCreateMeetings
+    policy.propagate_policy = msg.propagatePolicy
+    policy.parent_visible = msg.parentVisible
+    policy.propagate_admin = msg.propagateAdmin
     return policy
