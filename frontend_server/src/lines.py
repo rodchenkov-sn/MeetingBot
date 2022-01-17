264cfdf8fd26cdf1cb9984110e9e6e7625c15620
Artur Mkrtchian <deprecatednull@gmail.com>
2022-01-17 19:48:35 +0300

    Update: refactor LinesRepo

--- frontend_server/src/lines.py
+++ frontend_server/src/lines.py
@@ -1,38 +1,61 @@
-from typing import Optional
+import yaml
+
+from pymongo import MongoClient
+from typing import Optional, List
 
 
 class LinesRepo:
     def __init__(self) -> None:
-        from pymongo import MongoClient
-        cluster = ""
+        with open('config.yml', 'r') as config_file:
+            config = yaml.safe_load(config_file)
+        cluster = config['mongodb_cluster']
         client = MongoClient(cluster)
         db = client['localization']
-        self.__languages = db['languages']
+        self.__users = db['users']
         self.__lines = db['lines']
+        self.__languages = db['languages']
 
     def get_line(self, _name: str, _user_id: int) -> str:
         language = self.get_user_language(_user_id)
-        line = self.__lines.find_one( { '_id': _name } )
-        if language is None or line is None:
+        line_doc = self.__lines.find_one({'_id': _name})
+        if language is None or line_doc is None:
             return ''
         else:
-            translations = line['translations']
-            for translation in translations:
-                if translation['language'] == language:
-                    return translation['text']
+            translations = line_doc['translations']
+            for transl in translations:
+                if transl['language'] == language:
+                    line = transl['text']
+                    return line
 
     def get_user_language(self, _user_id: int) -> Optional[str]:
-        user_language = self.__languages.find_one( { '_id': _user_id } )
-        if user_language is None:
-            self.__languages.insert_one( { '_id': _user_id, 'language': 'en' } )
-            user_language = self.__languages.find_one( { '_id': _user_id } )
-        if user_language is None:
+        user_doc = self.__users.find_one({'_id': _user_id})
+        if user_doc is None:
+            self.__users.insert_one({'_id': _user_id, 'language': 'en'})
+            user_doc = self.__users.find_one({'_id': _user_id})
+        if user_doc is None:
             return None
         else:
-            return user_language['language']
+            user_language = user_doc['language']
+            return user_language
 
-    def update_user_language(self, _user_id: int, _language: str):
-        self.__languages.update_one(
-            { '_id': _user_id },
-            { "$set": { 'language': _language } }
-        )
+    def update_user_language(self, _user_id: int, _language: str) -> Optional[str]:
+        languages = self.get_all_languages()
+        if _language in languages:
+            self.__users.update_one(
+                {'_id': _user_id},
+                {"$set": {'language': _language}}
+            )
+            updated_language = self.get_user_language(_user_id)
+            return updated_language
+        else:
+            return None
+
+    def get_all_languages(self) -> Optional[List[str]]:
+        language_docs = self.__languages.find()
+        languages = []
+        if language_docs is None:
+            return None
+        else:
+            for l_d in language_docs:
+                languages.append(l_d['_id'])
+            return languages
