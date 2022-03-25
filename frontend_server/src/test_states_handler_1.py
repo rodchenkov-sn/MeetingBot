7ddef4c5f1780863625954d624b4cbb9d1c467f4
Artur Mkrtchyan <almond.pitch@mail.ru>
2022-03-25 11:01:54 +0300

    add assert to unit test for states handler class

--- frontend_server/src/test_states_handler_1.py
+++ frontend_server/src/test_states_handler_1.py
@@ -13,8 +13,9 @@ DEFAULT_USER_ID = 1
 
 DEFAULT_TEAM_NAME = "team1"
 
-START_CMD = "/start"
 CREATE_TEAM_CMD = "/create_team"
+
+CREATING_TEAM_STATE = "creating_team"
 
 
 class BackendServiceStub:
@@ -67,6 +68,8 @@ def test_try_handle_state_action_in_handlers(shr):
     )
     state = State(_action="creating_team", _argument=-1)
     r = list(shr.handler.try_handle(request, state))
+    assert shr.states.get_state(DEFAULT_USER_ID).action == CREATING_TEAM_STATE
+    assert shr.states.get_state(DEFAULT_USER_ID).argument == -1
     r = r.pop()
     assert r.user_id == DEFAULT_USER_ID
     create_team_enter_name = shr.lines.get_line('create_team_enter_name', DEFAULT_USER_ID)
