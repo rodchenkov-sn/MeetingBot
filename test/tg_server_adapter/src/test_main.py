47fe9f54a830a467f55756b46e68dc3a59303d1d
Arthur Mkrtchyan <almond.pitch@mail.ru>
2022-04-16 01:03:26 +0300

    move non-test functions to the top

--- test/tg_server_adapter/src/test_main.py
+++ test/tg_server_adapter/src/test_main.py
@@ -54,6 +54,141 @@ USERNAME_8 = "Nastya"
 USERNAME_9 = "Miya"
 
 
+def create_team(
+    username,
+    user_id,
+    team_name
+):
+    client = Client(username, user_id)
+
+    client.send_message('/create_team')
+    resp = responses_queue.get(timeout=10)
+    assert resp.user_id == user_id
+    assert resp.text == "Enter name:"
+
+    client.send_message(team_name)
+    resp = responses_queue.get(timeout=10)
+    assert resp.user_id == user_id
+    assert resp.text == f"{team_name} team created!"
+    resp = responses_queue.get(timeout=10)
+    assert resp.user_id == user_id
+    assert resp.text == LINE_HELP_EN
+
+
+def invite_to_team(
+    username,
+    user_id,
+    team_name,
+    invited_username,
+    invited_user_id
+) -> str:
+    pattern_team_id = re.compile(rf"/invite_member[0-9]+ -- {team_name}\n")
+
+    create_team(
+        username,
+        user_id,
+        team_name
+    )
+
+    client = Client(username, user_id)
+    invited_client = Client(invited_username, invited_user_id)
+
+    client.send_message("/invite_member")
+    resp = responses_queue.get(timeout=10)
+    assert resp.user_id == user_id
+    assert pattern_team_id.match(resp.text)
+
+    team_id = resp.text
+    team_id = re.sub("/invite_member", "", team_id)
+    team_id = re.sub(f" -- {team_name}\n", "", team_id)
+
+    client.send_message(f"/invite_member{team_id}")
+    resp = responses_queue.get(timeout=10)
+    assert resp.user_id == user_id
+    assert resp.text == "Tag one or multiple users"
+
+    client.send_message(f"@{invited_username}")
+    resp = responses_queue.get(timeout=10)
+    assert resp.user_id == invited_user_id
+    assert resp.text == f'You were invited to team {team_name} by @{username}\n\n/accept_invite{team_id} -- accept\n/reject_invite{team_id} -- reject'
+    resp = responses_queue.get(timeout=10)
+    assert resp.user_id == user_id
+    assert resp.text == "Invitations were send"
+    resp = responses_queue.get(timeout=10)
+    assert resp.user_id == user_id
+    assert resp.text == LINE_HELP_EN
+
+    return team_id
+
+
+def accept_team_invite(
+    username,
+    user_id,
+    team_name,
+    invited_username,
+    invited_user_id
+):
+    team_id = invite_to_team(
+        username,
+        user_id,
+        team_name,
+        invited_username,
+        invited_user_id
+    )
+
+    invited_client = Client(invited_username, invited_user_id)
+
+    invited_client.send_message(f"/accept_invite{team_id}")
+    resp = responses_queue.get(timeout=10)
+    assert resp.user_id == invited_user_id
+    assert resp.text == "Accepted!"
+    resp = responses_queue.get(timeout=10)
+    assert resp.user_id == user_id
+    assert resp.text == f"@{invited_username} accepted your invitation"
+
+
+def create_meeting(
+    username,
+    user_id,
+    team_name,
+    meeting_desc,
+    meeting_time_str
+):
+    pattern_team_id = re.compile(rf"/create_meeting[0-9]+ -- {team_name}\n")
+
+    create_team(
+        username,
+        user_id,
+        team_name
+    )
+
+    client = Client(username, user_id)
+
+    client.send_message('/create_meeting')
+    resp = responses_queue.get(timeout=10)
+    assert resp.user_id == user_id
+    assert pattern_team_id.match(resp.text)
+
+    team_id = resp.text
+    team_id = re.sub("/create_meeting", "", team_id)
+    team_id = re.sub(f" -- {team_name}\n", "", team_id)
+
+    client.send_message(f"/create_meeting{team_id}")
+    resp = responses_queue.get(timeout=10)
+    assert resp.user_id == user_id
+    assert resp.text == "Enter description:"
+
+    client.send_message(meeting_desc)
+    resp = responses_queue.get(timeout=10)
+    assert resp.user_id == user_id
+    assert resp.text == "Enter datetime (in format DD-MM-YYYY HH:MM):"
+
+    client.send_message(meeting_time_str)
+    resp = responses_queue.get(timeout=10)
+    assert resp.user_id == user_id
+    assert resp.text == "Meeting created!"
+
+
 @pytest.fixture(scope='session', autouse=True)
 def serv_starter():
     start_server()
@@ -105,79 +240,12 @@ def test_choose_language_cmd(serv_starter):
     assert resp.text == LINE_HELP_RU
 
 
-def create_team(
-    username,
-    user_id,
-    team_name
-):
-    client = Client(username, user_id)
-
-    client.send_message('/create_team')
-    resp = responses_queue.get(timeout=10)
-    assert resp.user_id == user_id
-    assert resp.text == "Enter name:"
-
-    client.send_message(team_name)
-    resp = responses_queue.get(timeout=10)
-    assert resp.user_id == user_id
-    assert resp.text == f"{team_name} team created!"
-    resp = responses_queue.get(timeout=10)
-    assert resp.user_id == user_id
-    assert resp.text == LINE_HELP_EN
-
-
 def test_create_team(serv_starter):
     create_team(
         username=USERNAME_4,
         user_id=USER_ID_4,
         team_name="konoha"
     )
-
-
-def invite_to_team(
-    username,
-    user_id,
-    team_name,
-    invited_username,
-    invited_user_id
-) -> str:
-    pattern_team_id = re.compile(rf"/invite_member[0-9]+ -- {team_name}\n")
-
-    create_team(
-        username,
-        user_id,
-        team_name
-    )
-
-    client = Client(username, user_id)
-    Client(invited_username, invited_user_id)
-
-    client.send_message("/invite_member")
-    resp = responses_queue.get(timeout=10)
-    assert resp.user_id == user_id
-    assert pattern_team_id.match(resp.text)
-
-    team_id = resp.text
-    team_id = re.sub("/invite_member", "", team_id)
-    team_id = re.sub(f" -- {team_name}\n", "", team_id)
-
-    client.send_message(f"/invite_member{team_id}")
-    resp = responses_queue.get(timeout=10)
-    assert resp.user_id == user_id
-    assert resp.text == "Tag one or multiple users"
-
-    client.send_message(f"@{invited_username}")
-    resp = responses_queue.get(timeout=10)
-    assert resp.user_id == invited_user_id
-    assert resp.text == f'You were invited to team {team_name} by @{username}\n\n/accept_invite{team_id} -- accept\n/reject_invite{team_id} -- reject'
-    resp = responses_queue.get(timeout=10)
-    assert resp.user_id == user_id
-    assert resp.text == "Invitations were send"
-    resp = responses_queue.get(timeout=10)
-    assert resp.user_id == user_id
-    assert resp.text == LINE_HELP_EN
-
-    return team_id
 
 
 def test_invite_to_team(serv_starter):
@@ -190,32 +258,6 @@ def test_invite_to_team(serv_starter):
     )
 
 
-def accept_team_invite(
-    username,
-    user_id,
-    team_name,
-    invited_username,
-    invited_user_id
-):
-    team_id = invite_to_team(
-        username,
-        user_id,
-        team_name,
-        invited_username,
-        invited_user_id
-    )
-
-    invited_client = Client(invited_username, invited_user_id)
-
-    invited_client.send_message(f"/accept_invite{team_id}")
-    resp = responses_queue.get(timeout=10)
-    assert resp.user_id == "Accepted!"
-    assert resp.text == invited_user_id
-    resp = responses_queue.get(timeout=10)
-    assert resp.user_id == user_id
-    assert resp.text == f"@{invited_username} accepted your invitation"
-
-
 def test_accept_team_invite(serv_starter):
     accept_team_invite(
         username=USERNAME_8,
@@ -224,48 +266,6 @@ def test_accept_team_invite(serv_starter):
         invited_username=USERNAME_9,
         invited_user_id=USER_ID_9
     )
-
-
-def create_meeting(
-    username,
-    user_id,
-    team_name,
-    meeting_desc,
-    meeting_time_str
-):
-    pattern_team_id = re.compile(rf"/create_meeting[0-9]+ -- {team_name}\n")
-
-    create_team(
-        username,
-        user_id,
-        team_name
-    )
-
-    client = Client(username, user_id)
-
-    client.send_message('/create_meeting')
-    resp = responses_queue.get(timeout=10)
-    assert resp.user_id == user_id
-    assert pattern_team_id.match(resp.text)
-
-    team_id = resp.text
-    team_id = re.sub("/create_meeting", "", team_id)
-    team_id = re.sub(f" -- {team_name}\n", "", team_id)
-
-    client.send_message(f"/create_meeting{team_id}")
-    resp = responses_queue.get(timeout=10)
-    assert resp.user_id == user_id
-    assert resp.text == "Enter description:"
-
-    client.send_message(meeting_desc)
-    resp = responses_queue.get(timeout=10)
-    assert resp.user_id == user_id
-    assert resp.text == "Enter datetime (in format DD-MM-YYYY HH:MM):"
-
-    client.send_message(meeting_time_str)
-    resp = responses_queue.get(timeout=10)
-    assert resp.user_id == user_id
-    assert resp.text == "Meeting created!"
 
 
 def test_create_meeting(serv_starter):
